71336a1318e153f3dedcecece0fa50ac71bcb939
LeonardoSSev <leonardossev@gmail.com>
2017-10-11 20:00:45 -0300

    Adiciona JWT para autenticação de usuário

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+class AuthenticationController extends Controller
+{
+     public function check_login(Request $request)
+    {
+    	$credentials = $request->only('email', 'password');
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response(trans('auth.failed'),401);
+            }
+        } catch (JWTException $e) {
+            return response(trans('auth.could_not_create_token'),500);
+        }
+
+        return response(compact('user', 'token'),200);
+    }
+
+    public function refresh_token(Request $request)
+    {
+        $token = JWTAuth::getToken();
+
+        if (!$token) {
+            return response()->json([
+                'error' => true,
+                'message' => ['Token não encontrado']
+            ], 400);
+        }
+
+        if ($token->get()[0] == ':' && $token->get()[1] == ' ') {
+            $token = substr($token->get(), 2);
+        }
+
+        try {
+            $token = JWTAuth::refresh($token);
+        } catch (TokenInvalidException $e) {
+            return response()->json([
+                'error' => true,
+                'message' => [trans('auth.access_denied')]
+            ], 401.3);
+        }
+
+        return response(compact('token'),200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +12,25 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('auth/login', 'AuthenticationController@check_login');
+Route::get('auth/refresh', 'AuthenticationController@refresh_token');
+
+Route::group(['middleware' => 'jwt.auth:api'], function(){
+	Route::get('/auth/teste', function(){
+		$user = JWTAuth::parseToken()->authenticate();
+		return [
+			'teste' => 'testado', 
+			'dsad'  => $user
+		];
+	});
 });
+
+
+
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+
+
+
+
