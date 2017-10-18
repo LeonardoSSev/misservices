70d45bf846dcf9afbad1435449043397ebbcbcd7
LeonardoSSev <leonardossev@gmail.com>
2017-10-18 01:21:03 -0200

    Adiciona arquivos no Angular para autenticação

---
+++ resources/assets/angular/src/app/models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    nome: string;
+    email: string;
+    password: string;
+    cpf: string;
+    uf: string;
+    cidade: string;
+    cep: string;
+    bairro: string;
+    endereco: string;
+    idUserType: number;
+}
--- resources/assets/angular/src/app/pagina-login/pagina-login.component.ts
+++ resources/assets/angular/src/app/pagina-login/pagina-login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../services/index';
+
 
 @Component({
   selector: 'app-pagina-login',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagina-login.component.css']
 })
 export class PaginaLoginComponent implements OnInit {
+    model: any = {};
+    loading = false;
+    error = '';
 
-  constructor() { }
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.logout();
+    }
 
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(result => {
+                if (result === true) {
+                    this.router.navigate(['/']);
+                } else {
+                    this.error = 'CPF ou senha está incorreto(a)';
+                    this.loading = false;
+                }
+            });
+    }
 }
