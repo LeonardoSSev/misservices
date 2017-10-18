d0a8fc02362ba2682990b9a220a9927f6cd758d8
Jonatas Lopes <jonatas_lopes@outlook.com>
2017-10-17 22:45:06 -0200

    estrutura inicial

---
+++ resources/views/index.blade.php
@@ -0,0 +1,211 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <title>MisServices</title>
+  <base href="/">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+    
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">MisServices</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#inicio">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#como-funciona">Como Funciona</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#categorias">Categorias</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#cadastro">Cadastre-se</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Acesse</a>
+          </li>
+        </ul>
+        <form class="form-inline mt-2 mt-md-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Pesquise um serviço..." aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+        </form>
+      </div>
+    </nav>
+
+    <div class="container marketing">
+    <div id="inicio">
+        <h1 class="digita">Bem Vindo ao MisServices</h1>
+        <div class="row">
+            <div class="col-lg-4">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+            <h2>Pesquise</h2>
+            <p>Encontre aqui os melhores profissionais na área que você deseja</p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+            <h2>Avalie</h2>
+            <p>Aponte a qualidade de um serviço prestado</p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+            <h2>Anuncie</h2>
+            <p>Seja um profissional no MisServices e ganhe mais!</p>
+            </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+    </div>
+
+    <div id="como-funciona">
+    <div class="row">
+                <h1>Como Funciona</h1>
+                <div class="row justify-content-md-center">
+                    <div class="card col-md-2 col-md-auto">
+                        <img src="cadastrar.png" alt="">
+                        <p>Primeiro você se cadastra</p>
+                    </div>
+                    <div class="card col-md-2 col-md-auto">
+                        <img src="profissional.png" alt="">
+                        <p>Então opta se também quer ser um profissional ou não</p>
+                    </div>
+                    <div class="card col-md-2 col-md-auto ">
+                        <img src="procurar.png" alt="">
+                        <p>
+                            Agora é só pesquisar o serviço que deseja e selecionar o profissional
+                        </p>
+                    </div>
+                    <div class="card col-md-2 col-md-auto">
+                        <img src="agendar.png" alt="">
+                        <p>Agendar o serviço</p>
+                    </div>
+                    <div class="card col-md-2 col-md-auto">
+                        <img src="avaliar.png" alt="">
+                        <p>E assim que terminar avaliar o serviço</p>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+     <!-- START THE FEATURETTES -->
+
+     <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Pesquise <span class="text-muted">Encontre aqui os melhores profissionais na área que você deseja</span></h2>
+          <p class="lead">Após realizar seu cadastro, você pode pesquisar os profissionais que
+              realizam aquilo que você precisa. Você é totalmente livre para filtrar os resultados
+              como quiser e escolher o profissional que achar melhor
+          </p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 order-md-2">
+          <h2 class="featurette-heading">Avalie <span class="text-muted">Aponte a qualidade de um serviço prestado</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5 order-md-1">
+          <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Anuncie <span class="text-muted">Seja um profissional no MisServices e ganhe mais!</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
+
+    <div id="categorias">
+    <h1>Principais Categorias</h1>
+        <div class="row">
+            <div class="col-lg-4">
+                <h1>Cat1</h1>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+                <h1>Cat2</h1>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+                <h1>Cat3</h1>
+            </div><!-- /.col-lg-4 -->
+            <button class="btn btn-secondary btn-block">Veja mais &raquo;</button>
+        </div><!-- /.row -->
+      </div>
+
+
+      <div id="cadastro">
+        <h1>Cadastre-se</h1>
+            <form method="post" action="" class="align-self-start">
+                <input type="text" placeholder="Nome" class="box form-control">
+                <input type="email" placeholder="E-mail" class="box form-control">
+                <input type="text" placeholder="CPF" class="box form-control">
+                <select name="" id="UF" class="box form-control">
+                    <option value="">Selecione o seu estado</option>
+                    <option value="">ES</option>
+                    <option value="">MG</option>
+                    <option value="">SP</option>
+                    <option value="">RJ</option>
+                </select>
+                <input type="text" placeholder="Cidade" class="box form-control">
+                <input type="text" placeholder="CEP" class="box form-control">
+                <input type="text" placeholder="Bairro" class="box form-control">
+                <input type="text" placeholder="Endereço" class="box form-control">
+
+                <button class="btn btn-block btn-success">Cadastrar</button>
+            </form>
+        </div>
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2017 MisServices, Inc. &middot; <a href="#">Termos</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/inline.bundle.js"></script>
+<script type="text/javascript" src="js/polyfills.bundle.js"></script>
+<script type="text/javascript" src="js/styles.bundle.js"></script>
+<script type="text/javascript" src="js/vendor.bundle.js"></script>
+<script type="text/javascript" src="js/main.bundle.js"></script>
+
+<script>
+var $doc = $('html, body');
+$('a').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top -150
+    }, 500);
+    return false;
+});
+</script>
+
+</body>
+
+</html>
