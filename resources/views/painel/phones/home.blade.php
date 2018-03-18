3f9d92ce10ecf26bfd83fd8a6afafc0609c2fd20
Leonardo Santos Severino <leonardossev@gmail.com>
2018-03-18 03:04:09 -0300

    [ADD] home for phones on admin panel

---
+++ resources/views/painel/phones/home.blade.php
@@ -0,0 +1,38 @@
+@extends('templates.temp')
+<!-- MENU ADMIN-->
+<div>
+    @include('painel.templates.menu-admin')
+    <main class="painel container">
+        <h1>Painel de Administração</h1>
+        <a href="/admin/phone/create">Adicionar</a>
+        <input type="text" class="box box-desk radius" placeholder="Pesquise um telefone..." number="pesquisar">
+        <table class="table">
+            <thead>
+                <th>ID</th>
+                <th>DDD</th>
+                <th>Número</th>
+                <th>Tipo de Telefone</th>
+                <th width="150px">Ações</th>
+            </thead>
+            <tbody>
+                @foreach($phones as $phone)
+                    <tr>
+                        <div class="loading"></div>
+                        <td>{{ $phone->id }}</td>
+                        <td>{{ $phone->ddd }}</td>
+                        <td>{{ $phone->number }}</td>
+                        <td>{{ $phone->phoneType->name }}</td>
+                        <td>
+                            <a href="/admin/phone/users/{{ $phone->id }}"><i class="fa fa-lg fa-phone" title="Visualizar usuários que usam {{ $phone->number }}"></i></a>
+                            <a href="/admin/phone/edit/{{ $phone->id }}"><i class="fa fa-lg fa-pencil" title="Editar {{ $phone->number }}"></i></a>
+                            <a href="/admin/phone/delete/{{ $phone->id }}"><i class="fa fa-lg fa-times" title="Excluir {{ $phone->number }}"></i></a>
+                            <a href="/admin/phone/view/{{ $phone->id }}"><i class="fa fa-lg fa-eye" title="Visualizar {{ $phone->number }}"></i></a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </main>
+</div>
+
+@include('templates.sections.footer')
