d212e036c0f28bfb847a1d302b1395abefefbe09
Leonardo Santos Severino <leonardossev@gmail.com>
2018-03-18 01:15:50 -0300

    [ADD] Index for types of phone on panel admin

---
+++ resources/views/painel/phoneTypes/home.blade.php
@@ -0,0 +1,36 @@
+@extends('templates.temp')
+<!-- MENU ADMIN-->
+<div>
+    @include('painel.templates.menu-admin')
+    <main class="painel container">
+        <h1>Painel de Administração</h1>
+        <a href="/admin/phone_type/create">Adicionar</a>
+        <input type="text" class="box box-desk radius" placeholder="Pesquise um tipo de telefone..." name="pesquisar">
+        <table class="table">
+            <thead>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th width="150px">Ações</th>
+            </thead>
+            <tbody>
+                @foreach($phoneTypes as $phone_type)
+                    <tr>
+                        <div class="loading"></div>
+                        <td>{{ $phone_type->id }}</td>
+                        <td>{{ $phone_type->name }}</td>
+                        <td>{{ $phone_type->description }}</td>
+                        <td>
+                            <a href="/admin/phone_type/phones/{{ $phone_type->id }}"><i class="fa fa-lg fa-phone" title="Visualizar tipos de telefone que são {{ $phone_type->name }}"></i></a>
+                            <a href="/admin/phone_type/edit/{{ $phone_type->id }}"><i class="fa fa-lg fa-pencil" title="Editar {{ $phone_type->name }}"></i></a>
+                            <a href="/admin/phone_type/delete/{{ $phone_type->id }}"><i class="fa fa-lg fa-times" title="Excluir {{ $phone_type->name }}"></i></a>
+                            <a href="/admin/phone_type/view/{{ $phone_type->id }}"><i class="fa fa-lg fa-eye" title="Visualizar {{ $phone_type->name }}"></i></a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </main>
+</div>
+
+@include('templates.sections.footer')
