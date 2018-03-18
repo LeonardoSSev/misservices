dfca9aab4692458c760a74c4e562f276910c3a5c
Leonardo Santos Severino <leonardossev@gmail.com>
2018-03-17 22:20:49 -0300

    [ADD] Models for phone and phone type

---
+++ app/Models/Phone.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Phone extends Model
+{
+    public function users()
+    {
+        return $this->belongsToMany(\App\User::class);
+    }
+
+    public function phoneType()
+    {
+        return $this->belongsTo(\App\PhoneType::class);
+    }
+}
