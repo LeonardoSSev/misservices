a4f8047af9851f98467cf67a4dd46f5348aada46
Leonardo Santos Severino <leonardossev@gmail.com>
2018-03-17 22:17:29 -0300

    [ADD] Helper class with some helper methods

---
+++ app/Helper.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Helper extends Model
+{
+
+    public function numberGenerator($maxLength):string
+    {
+        return substr(str_shuffle('12345678901'),0, $maxLength);
+    }
+
+
+    public function phoneGenerator($type):string
+    {
+
+        if ($type === 'cellphone') {
+            $phone = $this->numberGenerator(9);
+            $phone = substr_replace($phone, '9', 0, 1);
+            $phone = preg_replace("/([0-9]{5})/", "$1-$2", $phone);
+        } else {
+            $phone = $this->numberGenerator(8);
+            $phone = preg_replace("/([0-9]{4})/", "$1-$2", $phone);
+        }
+
+        return $phone;
+    }
+
+}
