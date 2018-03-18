d1cf92b8076cfaad5ef1ec0d2bfddb95016575a5
Leonardo Santos Severino <leonardossev@gmail.com>
2018-03-17 22:17:52 -0300

    [ADD] Migration for Phones

---
+++ database/migrations/2018_03_17_232941_create_phones_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ddd')->default('13');
+            $table->string('number', 20);
+            $table->integer('phone_types_id')->unsigned();
+            $table->foreign('phone_types_id')
+                ->references('id')
+                ->on('phone_types')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('phone_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('phone_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+
+            $table->foreign('phone_id')
+                ->references('id')
+                ->on('phones')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phone_user');
+        Schema::dropIfExists('phones');
+    }
+}
