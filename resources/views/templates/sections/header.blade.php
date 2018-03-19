9c943793fae6a99373167ce94d4759909d6f0e00
Leonardo Santos Severino <leonardossev@gmail.com>
2018-03-18 22:46:28 -0300

    [UPDATE] change for using the asset() to set the rote of the logo

--- resources/views/templates/sections/header.blade.php
+++ resources/views/templates/sections/header.blade.php
@@ -15,7 +15,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#page-top"> <img src="images/logo-misservices.png" alt="log"> </a>
+            <a class="navbar-brand" href="#page-top"> <img src=" {{ asset('images/logo-misservices.png') }}" alt="log"> </a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
