09617673045e92b7d82febf1a136c00eed6e7002
Jonatas Lopes <jonatasfeijolopes@outlook.com>
2018-03-31 15:46:39 -0300

    [MERGE]

--- public/js/app.js
+++ public/js/app.js
@@ -2130,12 +2130,6 @@ $(document).ready(function() {
 });
 $(document).ready(function(){
 
-    // Mascaras Cadastre-se
-    $('#telephone').mask('(00)0000-0000');
-    $('#celphone').mask('(00)00000-0000');
-    $('#cpf').mask('000.000.000-00');
-    $('#zipcode').mask('00000-000');
-
     // Index
     $('.slider.owl-carousel').owlCarousel({
         loop:true,
@@ -2154,15 +2148,12 @@ $(document).ready(function(){
         autoplayHoverPause:true
     });
 
-<<<<<<< HEAD
     // Mascaras Cadastre-se
     $('#telephone').mask('(00)0000-0000');
     $('#cellphone').mask('(00)00000-0000');
     $('#cpf').mask('000.000.000-00');
     $('#zipcode').mask('00000-000');
 
-=======
->>>>>>> 23ac3fdb5d6dfadff15b6346bbcfc74304a02885
     //Como Funciona
     $('.cliente').click(function(){
         $('.prestador').removeClass('active');
