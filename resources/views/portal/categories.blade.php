0845f0b933fddc926dde9e3e6015d2551a680934
Leonardo Santos Severino <leonardossev@gmail.com>
2018-04-21 02:29:07 -0300

    Add view for categories and services searches

--- resources/views/portal/categories.blade.php
+++ resources/views/portal/categories.blade.php
@@ -1,13 +1,15 @@
 @extends('templates.temp')
 @include('templates.sections.header')
 
-@foreach ($services as $service)
+
+<h2 style="padding-top: 10%; padding-left: 10%;">Buscar por categorias</h2>
+@foreach ($categories as $category)
     <div style="border: 2px black solid; boorder-radius: 5%; margin: 10%;">
-        <p>{{$service->cat_name}}</p>
-        <p>{{$service->name}}</p>
-        <p>{{$service->description}}</p>
-        <p>{{$service->user_id}}</p>
-        <p>{{$service->user_name}}</p>
+        <p>{{$category->name}}</p>
+        <p>{{$category->description}}</p>
+        <a href="{{route('user.search.services', [$category->id])}}">Ver serviços sobre {{$category->name}}</a>
     </div>
 @endforeach
+{!! $categories->links() !!}
+
 @include('templates.sections.footer')
---
+++ resources/views/portal/services.blade.php
@@ -0,0 +1,16 @@
+@extends('templates.temp')
+@include('templates.sections.header')
+
+
+<h2 style="padding-top: 10%; padding-left: 10%;">Buscar por {{$category->name}}</h2>
+<h5 style="padding-left: 10%;">Serviços relacionados a {{$category->name}}</h5>
+@foreach ($services as $service)
+    <div style="border: 2px black solid; boorder-radius: 5%; margin: 10%;">
+        <p>{{$service->name}}</p>
+        <p>{{$service->description}}</p>
+        <p>{{$service->user_name}}</p>
+    </div>
+@endforeach
+{!! $services->links() !!}
+
+@include('templates.sections.footer')
