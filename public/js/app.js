5a48267ec0fdfd71f8c07f60869ef93df5557934
Jonatas Lopes <jonatasfeijolopes@outlook.com>
2018-04-21 17:17:39 -0300

    [ALT] merge, resolução dos campos formatados no form de cadastro e padronização painel admin

--- public/js/app.js
+++ public/js/app.js
@@ -2133,21 +2133,12 @@ $(document).ready(function(){
         }
     });
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> d73f78e182670db9592458c96107d6fd4c3bd7aa
     // Mascaras Cadastre-se
     $('#telephone').mask('(00)0000-0000');
     $('#cellphone').mask('(00)00000-0000');
     $('#cpf').mask('000.000.000-00');
     $('#zipcode').mask('00000-000');
 
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 23ac3fdb5d6dfadff15b6346bbcfc74304a02885
->>>>>>> d73f78e182670db9592458c96107d6fd4c3bd7aa
     //Como Funciona
     $('.cliente').click(function(){
         $('.prestador').removeClass('active');
