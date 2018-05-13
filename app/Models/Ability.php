38ba2ac2cfd117b2c9744c6ced4e5b56910e6ca4
Leonardo Santos Severino <leonardossev@gmail.com>
2018-05-13 17:37:57 -0300

    Add ability model and migration

---
+++ app/Models/Ability.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ability extends Model
+{
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
